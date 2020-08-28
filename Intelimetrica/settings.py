bd02bfa356d897f1ef175a2ec4eb5ff21509418b
Gonzalo Romero <gonclapton@hotmail.com>
2020-08-28 02:53:29 -0500

    ADD django-heroku

--- Intelimetrica/settings.py
+++ Intelimetrica/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -139,3 +140,5 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
+
+django_heroku.settings(locals())
